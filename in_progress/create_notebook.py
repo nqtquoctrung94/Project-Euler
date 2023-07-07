15b975168664a25dc55ab43e06eea1093532c399
Quoc-Trung Nguyen <66000282+nqtquoctrung94@users.noreply.github.com>
2023-07-08 00:15:38 +0700

    commit

--- in_progress/create_notebook.py
+++ in_progress/create_notebook.py
@@ -5,13 +5,33 @@ from bs4 import BeautifulSoup
 class ProjectEuler_Notebook():
     def __init__(self, question_num):
         self.question_num = question_num
-        self.title = "problem_{:04d}".format(self.question_num)
+        self.name = "problem_{:04d}".format(self.question_num)
         self.notebook = nbf.v4.new_notebook()
 
+    def _get_question_title(self):
+        """This function use GET request to get the question's title from <head> - <title>.
+        There is no question title in minimal ver, so we have to access it from the main page
+        """
+        headers = {
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+        }
+        response = requests.get(f'https://projecteuler.net/problem={self.question_num}', headers=headers)
+
+        if response.ok:
+            soup = BeautifulSoup(response.text, "html.parser")
+            title = soup.find('head').find('title').get_text()
+            title = title.replace(f"{self.question_num}","") # Remove the head part
+            title = title.replace(" - Project Euler","") # Remove the tail part
+            return title
+        else:
+            print(response)
+            return None
+
     def _get_question_content(self):
-        '''This function use GET request to get question's content
-        So far I have not work on cleaning the url
-        '''
+        """This function use GET request to get question's content.
+        It is cleaner to access the minimal link, less cleaning to do.
+        So far I have not work on cleaning the url inside the content (some question require downloading the dataset)
+        """
         headers = {
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         }
@@ -20,19 +40,19 @@ class ProjectEuler_Notebook():
         if response.ok:
             soup = BeautifulSoup(response.text, "html.parser")
             soup_text = soup.get_text()
-            soup_cleaned = soup_text.replace("\n","<br>\n").replace("$<br>","$\n") # Disgusting code to clean string
-            return soup_cleaned
+            content = soup_text.replace("\n","<br>\n").replace("$<br>","$\n") # Disgusting code to clean string
+            return content
         else:
             print(response)
             return None
         
     def _fill_content(self):
-        '''Document: https://nbformat.readthedocs.io/en/latest/api.html#module-nbformat.v4'''
+        """Document: https://nbformat.readthedocs.io/en/latest/api.html#module-nbformat.v4"""
         
         self.notebook['cells'] = [
             # Title and question
             nbf.v4.new_markdown_cell(f"# [Question {self.question_num}](https://projecteuler.net/problem={self.question_num})"),
-            nbf.v4.new_markdown_cell(self._get_question_content()),
+            nbf.v4.new_markdown_cell(f"#{self._get_question_title()}\n{self._get_question_content()}"),
 
             # Solution 
             nbf.v4.new_markdown_cell("# Solution"),
@@ -45,6 +65,7 @@ class ProjectEuler_Notebook():
 
     def create(self):
         self._fill_content()
-        nbf.write(self.notebook, fr'in_progress\{self.title}.ipynb')
+        nbf.write(self.notebook, fr'in_progress\{self.name}.ipynb')
+        print(f"Successfully created notebook {self.name}")
 
-ProjectEuler_Notebook(10).create()
+ProjectEuler_Notebook(15).create()
--- python-solutions/euler_lib.py
+++ python-solutions/euler_lib.py
@@ -1,71 +1,89 @@
 # Prepared function that commonly used in solving problems
+# Sorted by alphabet
 
+from collections.abc import Iterator
 import math
- 
-def least_common_multiple(a:int, b:int) -> int:
-	'''This function return Least Common Multiple of a and b
-	Ref: https://en.wikipedia.org/wiki/Greatest_common_divisor#Least_common_multiple'''
-	return (a*b) // math.gcd(a,b)
 
+def divisors_count(n: int) -> int:
+    """This function return the number of divisors of n. The idea is based on divisors_list function"""
+    result = 0
+    for i in range(1, math.isqrt(n) + 1):
+        if n % i == 0:
+            if i * i == n:
+                result += 1
+            else:
+                result += 2
+    return result
+
+
+def divisors_list(n: int) -> Iterator[int]:
+    """This function return the generator of all divisors of n"""
+    for i in range(1, math.isqrt(n) + 1):
+        if n % i == 0:
+            if i * i == n:
+                yield i
+            else:
+                yield i
+                yield n//i
+
+def is_even(n: int) -> bool:
+    """This function check if number is even or not. By using result of n % 2 == 0"""
+    return (n % 2 == 0)
 
 def is_panlindromic(n: int) -> bool:
-    '''This function take in number n
-    Turn into string_n
-    Create reverse_n, ref: https://www.w3schools.com/python/python_howto_reverse_string.asp
-    Then compare string_n with reverse_n
-    '''
+    """This function check if number n is panlidromic
+    - Turn n into string
+    - Create reverse_n, ref: https://www.w3schools.com/python/python_howto_reverse_string.asp
+    - Then compare string_n with reverse_n
+    """
     string_n = str(n)
     reverse_n = string_n[::-1]    
     return (string_n == reverse_n)
 
-
 def is_prime(n: int) -> bool:
-    '''Ref: https://en.wikipedia.org/wiki/Primality_test#Python
+    """Ref: https://en.wikipedia.org/wiki/Primality_test#Python
     If we write the prime number in the form of (p = 6k + i), with i=[0,1,2,3,4,5]
     We can see that:
-        - with i=[0,2,4], 6k+i is divisible by 2
-        - with i=[0,3], 6k+i is divisible by 3
-    So first check if number are divisble by 2 and 3 first, then check for every p = 6k+1 and 6k+5
-    '''
+        - with i=[0,2,4] -- 6k+i is divisible by 2
+        - with i=[0,3] -- 6k+i is divisible by 3
+    So first check divisble by 2 and 3, then check for every p = 6k+1 and 6k+5
+    """
     if (n <= 3):
         return (n > 1)
-    
     if (n % 2 == 0) or (n % 3 == 0):
         return False
-    
     for i in range(5, math.isqrt(n)+1, 6):      # Loop for 6k + 5
         if (n % i == 0) or (n % (i+2) == 0):
             return False
-        
     return True
 
+def least_common_multiple(a:int, b:int) -> int:
+	"""This function return Least Common Multiple of a and b
+	Ref: https://en.wikipedia.org/wiki/Greatest_common_divisor#Least_common_multiple"""
+	return (a*b) // math.gcd(a,b)
 
 def one_to_n_sum(n: int) -> int:
-    '''Ref: https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+    """Ref: https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
     Proof: https://proofwiki.org/wiki/Closed_Form_for_Triangular_Numbers
     S(n) = 1 + 2 + 3 + ... + n = n*(n+1)/2
     Using n*(n+1)//2 seems to be quicker than int(n*(n+1)/2) since it does not involve handing float numbers
-    '''
+    """
     return n*(n+1)//2
 
-
 def one_to_n_square_sum(n: int) -> int:
-    '''S(n) = 1^2 + 2^2 + 3^2 + ... + n^2 = n*(n+1)*(2*n+1)/6 = (n**3)/3 + (n**2)/2 + n/6
+    """S(n) = 1^2 + 2^2 + 3^2 + ... + n^2 = n*(n+1)*(2*n+1)/6 = (n**3)/3 + (n**2)/2 + n/6
     Ref: https://en.wikipedia.org/wiki/Square_pyramidal_number
-    Proof: https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares'''
+    Proof: https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares"""
     return n*(n+1)*(2*n+1) // 6
 
-
-def Sieve_of_Eratosthenes(limit: int) -> list:
-    '''Ref: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+def sieve_of_eratosthenes(limit: int) -> list:
+    """Ref: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
     Ref 2: https://t5k.org/glossary/page.php?sort=SieveOfEratosthenes
     Return list of all prime below input 'n'
     consumes O(n) of the memory and performs O(n log log n)
-    '''
-
+    """
     # Create array of True value
     num_list = [True]*limit
-
     for i in range(2, math.isqrt(limit)+1):
         # if current index is prime (value = True)
         if num_list[i] == True:
@@ -74,9 +92,6 @@ def Sieve_of_Eratosthenes(limit: int) -> list:
             while j < limit:
                 num_list[j] = False
                 j += i
-    
     # Ramaining True indices are prime numbers
-    prime_list = [i for i in range(2,len(num_list)) if num_list[i] == True]
-
-    return prime_list
+    return [i for i in range(2, len(num_list)) if num_list[i] == True]
 
