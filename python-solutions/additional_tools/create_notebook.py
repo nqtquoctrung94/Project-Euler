29623bab39e4e43086958cf11a509d40e79d28da
Quoc-Trung Nguyen <66000282+nqtquoctrung94@users.noreply.github.com>
2023-07-25 16:15:12 +0700

    add P51, P53, P53

--- python-solutions/additional_tools/create_notebook.py
+++ python-solutions/additional_tools/create_notebook.py
@@ -68,4 +68,4 @@ class ProjectEuler_Notebook():
         nbf.write(self.notebook, fr'python-solutions\{self.name}.ipynb')
         print(f"Successfully created notebook {self.name}.ipynb")
 
-ProjectEuler_Notebook(50).create()
+ProjectEuler_Notebook(53).create()
