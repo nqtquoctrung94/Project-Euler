7475a2cd39fd01de66b5aac18dcf346c057432d6
Quoc-Trung Nguyen <66000282+nqtquoctrung94@users.noreply.github.com>
2023-09-18 01:31:08 +0700

    add greatest_common_divisor ,update comments

--- python-solutions/snippet/euler_lib.py
+++ python-solutions/snippet/euler_lib.py
@@ -28,7 +28,7 @@ def divisors_list(n: int) -> Iterator[int]:
 
 
 def divisors_sum_list(limit: int) -> list:
-    """This function return the divisors sum list from 0 to limit, for more indeep prove, check question 21"""
+    """This function return the divisors sum list from 0 to limit, for more indeep proof, check question 21"""
     sum_list = [0]*(limit+1)
     for divisor in range(1, limit//2 + 1):
         index = divisor
@@ -71,8 +71,8 @@ def fibonacci_recursive(position: int) -> int:
 
 def fibonacci_matrix(position: int) -> int:
     """This function return the Fibonacci number at nth position using the power of matrix
+    There are still some errors in the function.
     Reading: https://www.geeksforgeeks.org/matrix-exponentiation/
-    There are still some errors in the function which I don't know why
     """
     def multiply_matrix(matrix_a:list, matrix_b:list) -> list:
         # Create an auxiliary matrix to store elements after multiply
@@ -112,45 +112,126 @@ def fibonacci_matrix(position: int) -> int:
     return find_pos_n(position)
     
 
+def greatest_common_divisor_binary_module(a: int, b: int) -> int:
+    """ This function find GCD of a and b using binary algorithm, also known as Stein's algorithm.
+    The algorithm repeatedly applying these identities:
+    - gcd(0 , b) = b and gcd(a, 0) = a.
+    - gcd(2a,2b) = 2*gcd(a, b)
+    - gcd(2a, b) = gcd(a, 2b) = gcd(a, b)
+    - gcd(a , b) = gcd(|a − b|, min(a, b)), if a and b are both odd.
+
+    Reading: https://en.wikipedia.org/wiki/Binary_GCD_algorithm
+    """
+    # Base case
+    if a == 0: return b
+    if b == 0: return a
+
+    # Save the multipler of 2
+    power2 = 0
+    while (a % 2 == 0) and (b % 2 == 0):
+        a //= 2
+        b //= 2
+        power2 += 1
+    
+    # Remove factors of 2 from a
+    while (a % 2 == 0):
+        a //= 2
+
+    # Calculate GCD
+    while b != 0:
+        while (b % 2 == 0):
+            b //= 2
+        if a > b:
+            a, b = b, a
+        b -= a      # Shorter, but worse performance: a,b = min(a,b), abs(a-b)
+    return a * (2**power2)
+
+
+def greatest_common_divisor_binary_shift(a: int, b: int) -> int:
+    """ This function find GCD of a and b using binary algorithm, by applying bitwise operations.
+    The idea is the same as function greatest_common_divisor_binary_module
+
+    Reading: https://saturncloud.io/blog/binary-gcd-a-faster-algorithm-for-greatest-common-divisor/   
+    """
+    # Base case
+    if a == 0: return b
+    if b == 0: return a
+
+    # Save the multipler of 2
+    shift = 0
+    while ((a | b) & 1) == 0:
+        a >>= 1
+        b >>= 1
+        shift += 1
+    
+    # Remove factors of 2 from a
+    while (a & 1) == 0:
+        a >>= 1
+
+    # Calculate GCD
+    while b != 0:
+        while (b & 1) == 0:
+            b >>= 1
+        if a > b:
+            a, b = b, a
+        b -= a      # Shorter, but worse performance: a,b = min(a,b), abs(a-b)
+    return a << shift
+
+
+def greatest_common_divisor_recursive(a: int, b: int) -> int:
+    """ This function find GCD of a and b using Euclidean recursive algorithm.
+    Reading: https://en.wikipedia.org/wiki/Greatest_common_divisor#Euclidean_algorithm
+    """
+    if b == 0:
+        return a
+    else:
+        return greatest_common_divisor_recursive(b, a % b)
+
+
 def is_even(n: int) -> bool:
-    """This function check if number is even or not. By using result of n % 2 == 0"""
+    """This function check if number is even or not"""
     return (n % 2 == 0)
 
 
 def is_palindromic(n: int) -> bool:
-    """This function check if number n is panlidromic
+    """ This function check if number n is panlidromic, aka reading from left to right is the same with right to left
     - Turn n into string
-    - Create reverse_n, ref: https://www.w3schools.com/python/python_howto_reverse_string.asp
+    - Create reverse_n by apply slice notation list[<start>:<stop>:<step>]
     - Then compare string_n with reverse_n
+    Reading: https://www.w3schools.com/python/python_howto_reverse_string.asp
     """
     string_n = str(n)
     reverse_n = string_n[::-1]    
     return (string_n == reverse_n)
 
 def is_pentagonal_number(n: int) -> bool:
-    """Ref: https://en.wikipedia.org/wiki/Pentagonal_number#Tests_for_pentagonal_numbers
-    This function return if input number is a pentagonal number"""
+    """This function return if input number is a pentagonal number
+    Reading: https://en.wikipedia.org/wiki/Pentagonal_number#Tests_for_pentagonal_numbers
+    """
     if n < 1:
         return False
     x = (math.sqrt(24*n + 1)+1)/6
     return x.is_integer()
 
 def is_prime(n: int) -> bool:
-    """Ref: https://en.wikipedia.org/wiki/Primality_test#Python
-    If we write the prime number in the form of (p = 6k + i), with i=[0,1,2,3,4,5]
+    """ This function check if input number n is prime or not
+    Write the prime number in the form of p=6k+i, with i=[0,1,2,3,4,5]
     We can see that:
         - with i=[0,2,4] -- 6k+i is divisible by 2
-        - with i=[0,3] -- 6k+i is divisible by 3
+        - with i=[0,3]   -- 6k+i is divisible by 3
     So first check divisble by 2 and 3, then check for every p = 6k+1 and 6k+5
+    
+    Reading: https://en.wikipedia.org/wiki/Primality_test#Python
     """
     if (n <= 3):
         return (n > 1)
     if (n % 2 == 0) or (n % 3 == 0):
         return False
     for i in range(5, math.isqrt(n)+1, 6):      # Loop for 6k + 5
-        if (n % i == 0) or (n % (i+2) == 0):
+        if (n % i == 0) or (n % (i+2) == 0):    # i+2 = 6k+5+2 = 6(k+1) + 1
             return False
     return True
+
 
 def is_square(n: int) -> bool:
     """This function return if input number is square"""
@@ -159,38 +240,55 @@ def is_square(n: int) -> bool:
     rounded_square_n = math.isqrt(n)
     return rounded_square_n**2 == n
 
+
 def is_triangular_number(n: int) -> bool:
-    """Ref: https://en.wikipedia.org/wiki/Triangular_number#Triangular_roots_and_tests_for_triangular_numbers
-    This function will check if a number is a triangular number:
+    """ This function will check if a number is a triangular number:
     
     Triangular number T is defined as: 
     T(n) = 1 + 2 + 3 + ... + n = n*(n+1)/2
 
-    This function use Triangular test such that: T(n) = x => 8x + 1 is a square
+    This function use Triangular test:
+    - Let T(n) = x 
+    - 8x + 1 must be square number
+    
+    Prove:
+    8x + 1 = 8*n*(n+1)/2 + 1 = 4n(n+1) + 1 = 4(n^2) + 4n + 1 = (2n+1)^2
+
+    Reading:
+    https://en.wikipedia.org/wiki/Triangular_number#Triangular_roots_and_tests_for_triangular_numbers
     """
     return is_square(8*n+1)
 
+
 def least_common_multiple(a:int, b:int) -> int:
-	"""This function return Least Common Multiple of a and b
-	Ref: https://en.wikipedia.org/wiki/Greatest_common_divisor#Least_common_multiple"""
-	return (a*b) // math.gcd(a,b)
+    """This function return Least Common Multiple of a and b
+    Ref: https://en.wikipedia.org/wiki/Greatest_common_divisor#Least_common_multiple"""
+    return (a*b) // greatest_common_divisor_recursive(a,b)
+ 
 
 def one_to_n_sum(n: int) -> int:
-    """Ref: https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
-    Proof: https://proofwiki.org/wiki/Closed_Form_for_Triangular_Numbers
+    """ Calculate the sum of numbers from 1 to n:
     S(n) = 1 + 2 + 3 + ... + n = n*(n+1)/2
-    Using n*(n+1)//2 seems to be quicker than int(n*(n+1)/2) since it does not involve handing float numbers
+    Using n*(n+1)//2 is quicker than int(n*(n+1)/2) since it does not involve handing float numbers
+
+    Ref: https://en.wikipedia.org/wiki/1_%2B_2_%2B_3_%2B_4_%2B_%E2%8B%AF
+    Proof: https://proofwiki.org/wiki/Closed_Form_for_Triangular_Numbers
     """
     return n*(n+1)//2
 
+
 def one_to_n_square_sum(n: int) -> int:
-    """S(n) = 1^2 + 2^2 + 3^2 + ... + n^2 = n*(n+1)*(2*n+1)/6 = (n**3)/3 + (n**2)/2 + n/6
+    """ Calculate the sum of square numbers from 1 to n:
+    S(n) = 1^2 + 2^2 + 3^2 + ... + n^2 = n*(n+1)*(2*n+1)/6 
+
     Ref: https://en.wikipedia.org/wiki/Square_pyramidal_number
-    Proof: https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares"""
+    Proof: https://proofwiki.org/wiki/Sum_of_Sequence_of_Squares
+    """
     return n*(n+1)*(2*n+1) // 6
 
+
 def proper_divisors_list(n: int) -> Iterator[int]:
-    """This function return the proper divisors of n (number less than n which divide evenly into n)"""
+    """ This function return the proper divisors of n (number less than n which divide evenly into n)"""
     yield 1
     for i in range(2, math.isqrt(n) + 1):
         if n % i == 0:
@@ -200,8 +298,9 @@ def proper_divisors_list(n: int) -> Iterator[int]:
                 yield i
                 yield n//i
 
+
 def proper_divisors_sum_list(limit: int) -> list:
-    """This function return the divisors sum list from 0 to limit, for more indeep prove, check question 21"""
+    """ This function return the divisors sum list from 0 to limit, for more indeep prove, check question 21"""
     sum_list = [0]*(limit+1)
     for divisor in range(1, limit//2 + 1):
         index = divisor*2
@@ -210,18 +309,24 @@ def proper_divisors_sum_list(limit: int) -> list:
             index += divisor
     return sum_list
 
+
 def s_gonal_number(side: int, index: int) -> int:
     """ This function return the Polygonal number, given side and index
-    Input: side, index, with side >= 2, index >= 1
+    Input: side >= 2, index >= 1
     Output: P(side,index)
-    Ref: https://en.wikipedia.org/wiki/Polygonal_number"""
-    return (side - 2)*(index*(index-1)//2) + index
+    Reading: https://en.wikipedia.org/wiki/Polygonal_number
+    """
+    return (side-2)*(index*(index-1)//2) + index
+
 
 def sieve_of_eratosthenes(limit: int) -> list:
-    """Ref: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
-    Ref 2: https://t5k.org/glossary/page.php?sort=SieveOfEratosthenes
-    Return list of all prime below input 'n'
+    """ Return list of all prime below input 'n'
     consumes O(n) of the memory and performs O(n log log n)
+
+    Reading list:
+    - https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+    - https://t5k.org/glossary/page.php?sort=SieveOfEratosthenes
+    - https://cp-algorithms.com/algebra/sieve-of-eratosthenes.html
     """
     # Create array of True value
     num_list = [True]*limit
